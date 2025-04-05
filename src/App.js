206ed6d0c61ddf8023a8f8d88b0f0639424a7c70
mm04625 <mm04625@surrey.ac.uk>
2025-04-05 17:22:07 +0100

    added routes to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { useEffect } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductDetail from "./pages/product/ProductDetail";
+import { useProductsStore } from "./store/useProductStore";
 
 function App() {
+  const { products, isLoading, error, fetchData } = useProductsStore();
+
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+  
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+      {/* <Header> add header component here */}
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+        </Routes>
+      {/* <Header /> */}
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
