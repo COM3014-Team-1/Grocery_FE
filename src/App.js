53b99d0028784553b6bf6403731d481aa39bb778
mm04625 <mm04625@surrey.ac.uk>
2025-04-17 21:48:13 +0100

    Added routes to the app.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,33 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/product/ProductDetail";
-import { useProductsStore } from "./store/useProductStore";
+import CategoryPage from "./pages/Category/Category";
+import Home from "./pages/Home/Home";
+import useCategoryStore from "./store/useCategoryStore";
+import Header from "./components/Header";
 
 function App() {
-  const { products, isLoading, error, fetchData } = useProductsStore();
-
   const theme = createTheme({
     palette: {
       mode: "light",
     },
   });
-  
+
+  const { categories, fetchCategories } = useCategoryStore();
+
   useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    fetchCategories();
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-      {/* <Header> add header component here */}
+      <Header categories={categories} />
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
-          {/* <Route path="/products" products={products} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/category/:id" element={<CategoryPage/>} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
