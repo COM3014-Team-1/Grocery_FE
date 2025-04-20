2d03ae94181c35f94ed0c58ac56bd65a125ec3a3
rs02603 <rs02603@surrey.ac.uk>
2025-04-20 21:49:28 +0100

    Integrated Signup and Login Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/product/ProductDetail";
 import CategoryPage from "./pages/Category/CategoryPage";
+import AuthForm from "./AuthForm";  
+import SignupForm from "./SignupForm";  
 import Home from "./pages/Home/Home";
 import useCategoryStore from "./store/useCategoryStore";
 import Header from "./components/Header";
@@ -30,6 +32,8 @@ function App() {
       <BrowserRouter>
         <Header categories={categories} />
         <Routes>
+	  <Route path="/login" element={<AuthForm />} />
+	  <Route path="/signup" element={<SignupForm />} />
           <Route path="/" element={isLoading ? (<div>Home</div>) : (<Home categories={categories}/>)} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/category/:id" element={<CategoryPage />} />
