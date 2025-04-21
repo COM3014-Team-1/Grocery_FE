049e5cbd187547244f8f482e6eb0bd1fb6300378
mm04625 <mm04625@surrey.ac.uk>
2025-04-21 21:18:59 +0100

    setting userId to app state

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -5,6 +5,7 @@ export const useUserStore = create((set) => ({
     name: 'John Doe',
     email: 'john@example.com',
     avatar: '', // optional: add avatar URL if you want to show user image
+    userId: '7fe4a0ed-4206-4a77-9610-532d89fafc80'
   },
   setUser: (userData) => set({ user: userData }),
   logout: () => set({ user: null }),
