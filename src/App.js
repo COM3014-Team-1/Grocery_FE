88680e1fcdfba1b26a6ec714b1ceb29511da498f
Alice <ms04773@surrey.ac.uk>
2025-04-25 20:05:34 +0100

    bugfix: resolve logout issue on page refresh refactor: clean up related code style: UI adjustments

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/product/ProductDetail";
 import CategoryPage from "./pages/Category/CategoryPage";
-import AuthForm from "./pages/Login/AuthForm";  
-import SignupForm from "./pages/Signup/SignupForm";  
-import UserProfile from "./pages/UserProfile/Userprofile";
 import Home from "./pages/Home/Home";
 import useCategoryStore from "./store/useCategoryStore";
+import LoginPage from "./pages/Login/AuthForm";
+import SignupPage from "./pages/Signup/SignupForm";
 import Header from "./components/Header";
-import { Box, CircularProgress } from "@mui/material";
+import { Box, CircularProgress, Container } from "@mui/material";
+import ProfilePage from "./pages/UserProfile/Userprofile";
 
 function App() {
   const { categories, fetchCategories, isLoading } = useCategoryStore();
@@ -33,14 +33,12 @@ function App() {
       <BrowserRouter>
         <Header categories={categories} />
         <Routes>
-
-          <Route path="/" element={isLoading ? (<div>Home</div>) : (<Home categories={categories}/>)} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={isLoading ? (<Container><CircularProgress color="success" /></Container>) : (<Home categories={categories}/>)} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/category/:id" element={<CategoryPage />} />
-         <Route path="/login" element={<AuthForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route path="/profile" element={<UserProfile />} />
-
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
